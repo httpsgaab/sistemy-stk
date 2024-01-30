3a13d2ccd85c4eca525168566276b36738b7f021
Romulord93 <145913123+Romulord93@users.noreply.github.com>
2024-01-30 15:54:31 -0300

    Add files via upload
    Alguns dos códigos base testados para manipular o estoque

---
+++ verfc_att_tabela.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#código para verificar como anda a atualização da tabela:
+
+import pandas as pd
+estoque = pd.read_excel("estoque.xlsx")
+display(estoque)
+
+
+# In[ ]:
+
+
+
+
